aa5098a5d424e25b868a5a0c83f3921e494cecab
sumit kumar <sumit.saiwal@gmail.com>
2017-07-25 18:57:43 +0530

    Added SQL script

---
+++ DB/leavedb.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `adminsumit` (
+  `adminid` int(10) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  `adm_name` char(20) NOT NULL,
+  PRIMARY KEY (`adminid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
